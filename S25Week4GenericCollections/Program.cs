47279de9bf0c84eb3a67de118c411b2594f41e8e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-29 14:13:44 -0400

    array example

--- S25Week4GenericCollections/Program.cs
+++ S25Week4GenericCollections/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Array
+
+            int[] myArray = new int[4];
+            myArray[0] = 10;
+            myArray[1] = 20;
+            myArray[2] = 30;
+            myArray[3] = 40;
+
+            int sum = 0;
+
+            for (int i=0; i<myArray.Length; i++)
+            {
+                int num = myArray[i];
+                sum += num;
+            }
+
+            Console.WriteLine("Sum = " + sum);
         }
     }
 }
