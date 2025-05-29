de7904a3ab0f7fec2cc63137114548817784f88d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-29 14:59:32 -0400

    emp class

---
+++ ListExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
