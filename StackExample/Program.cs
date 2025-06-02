30c95af90222ee2be9e82e99ff44a7f923b6eacc
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-02 08:48:12 -0400

    stack example

---
+++ StackExample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StackExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            //int num = stack[1];
+
+            foreach (var i in stack)
+                Console.WriteLine(i);
+            
+
+            int num = stack.Pop();
+            Console.WriteLine("\n\nTotal items: " + stack.Count);
+            Console.WriteLine("num = " + num);
+
+
+            num= stack.Peek();
+            Console.WriteLine("\n\nTotal items: " + stack.Count);
+            Console.WriteLine("num = " + num);
+        }
+    }
+}
